5cba9979e078a4a08c19299e6bb9dd2db02df1b1
donthomasitos <github@thomaswillberger.com>
2024-06-28 12:32:02 +0200

    req change

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ REQUIRED = [
     "unidecode",
     "phonemizer",
     "torchlibrosa>=0.0.9",
-    "transformers==4.30.2",
+    "transformers",
     "huggingface_hub",
     "progressbar",
     "ftfy",
